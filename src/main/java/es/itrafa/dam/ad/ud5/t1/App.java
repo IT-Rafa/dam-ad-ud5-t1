8c52f35fab1ddbadcf0687c07e202d54218d3c5a
it-rafa <it-rafa@bit-acora.com>
2021-05-09 20:20:57 +0200

    init with maven-archetype-quickstart v1.4 - create package baseDatos

---
+++ src/main/java/es/itrafa/dam/ad/ud5/t1/App.java
@@ -0,0 +1,13 @@
+package es.itrafa.dam.ad.ud5.t1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
